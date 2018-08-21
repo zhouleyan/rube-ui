935423dd7bfa38a5d1d5f83112795d85eec8ebfb
zhouleyan <zly750@qq.com>
2018-08-21 16:15:54 +0800

    remove styled-components

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -8,24 +8,14 @@
 
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import styled from 'styled-components';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 
-const AppWrapper = styled.div`
-  max-width: calc(768px + 16px * 2);
-  margin: 0 auto;
-  display: flex;
-  min-height: 100%;
-  padding: 0 16px;
-  flex-direction: column;
-`;
-
 export default function App() {
   return (
-    <AppWrapper>
+    <div>
       <Helmet
         titleTemplate="%s - React.js Boilerplate"
         defaultTitle="React.js Boilerplate"
@@ -36,6 +26,6 @@ export default function App() {
         <Route exact path="/" component={HomePage} />
         <Route path="" component={NotFoundPage} />
       </Switch>
-    </AppWrapper>
+    </div>
   );
 }
