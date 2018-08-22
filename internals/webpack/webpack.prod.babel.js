2e9778f023241cbbbb561aa951628fddf1f1065e
zhouleyan <zly750@qq.com>
2018-08-22 17:14:55 +0800

    add sideEffects app/components

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -75,9 +75,9 @@ module.exports = require('./webpack.base.babel')({
     }),
 
     new WebpackPwaManifest({
-      name: 'React Boilerplate',
-      short_name: 'React BP',
-      description: 'My React Boilerplate-based project!',
+      name: 'Rube-UI',
+      short_name: 'Rube-UI',
+      description: 'My Rube-UI project!',
       background_color: '#fafafa',
       theme_color: '#b1624d',
       icons: [
